39e2d78bdfa84547e8b5a7d8b634b4ceead9c89e
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2023-01-31 12:00:48 +0200

    add alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
@@ -16,9 +16,46 @@ import Cart from "./components/pages/Cart";
 import ShippingAddress from "./components/pages/ShippingAddress";
 import PaymentMethod from "./components/pages/PaymentMethod";
 import { products } from "./_Data";
+import styled from "styled-components";
 
 
+const Alert = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  z-index: 1100;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  padding-inline: 20vw;
+  transform: translateY(-100%);
+  transition: all 500ms ease;
+  padding-block: 1rem;
 
+  p {
+    font-size: 1.1rem;
+  }
+  button {
+    background-color: #000;
+    color: #fff;
+
+    &:hover {
+      background-color: #fff;
+      color: #000;
+    }
+  }
+
+  @media(max-width: 992px) {
+    padding-inline: 1rem;
+  }
+  @media(max-width: 450px) {
+    p {
+      font-size: 13px;
+    }
+  }
+`
 
 function App() {
 
@@ -26,18 +63,34 @@ function App() {
   // const dispatch = useDispatch();
   // const productsReducer  = useSelector(state => state.productsReducer);
   // const { products } = productsReducer;
-  
-  
+
+
   // useEffect(() => {
 
   //   dispatch(handleInitialData());
   // }, [dispatch]);
-  
 
+  const [showAlert, setShowAlert] = useState(false);
+  useEffect(() => {
+    // setTimeout(() => {
+      document.getElementById("alert").style.transform = 'translateY(0%)';
+    // }, 1000)
+  }, []);
+  const hideAlert = () => {
+    document.getElementById("alert").style.transform = 'translateY(-100%)';
+  }
 
   return (
     <Router>
       <div className="App">
+        <Alert id="alert">
+          <p>
+            This is just a non-functional frontend template as the backend is temporarily off
+          </p>
+          <button onClick={hideAlert}>
+            close
+          </button>
+        </Alert>
         <ScrollToTop />
         <NavBar />
         <Routes>
